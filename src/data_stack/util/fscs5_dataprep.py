82f2b568e6c0cddc6394ad62ad113368040cc7e3
Lorenz Wickert <lorenz.wickert@iais.fraunhofer.de>
2021-06-09 08:05:33 +0000

    Finished implementing dataset converter

--- src/data_stack/util/fscs5_dataprep.py
+++ src/data_stack/util/fscs5_dataprep.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import hashlib
 
 class Dset_converter():
 
@@ -20,13 +21,13 @@ class Dset_converter():
     def __init__(self, input_name="Traffic_Complete_11_cut.hdf"):
         self.input_name = input_name
 
-    def convert_dataset(self):
+    def convert_dataset(self, verbose=False):
         # Load dataset
-        df = pd.read_hdf(os.path.join(self.DSET_Location, self.input_name))
+        df = pd.read_hdf(os.path.join(self.DSET_LOCATION, self.input_name))
 
         # Separate Normal / Anomalous data
-        df_normal = df.loc[df["Labels"] == 0]
-        df_anomalous = df.loc[df["Labels"] > 0]
+        df_normal = df.loc[df["labels"] == 0]
+        df_anomalous = df.loc[df["labels"] > 0]
 
         # Create Train / Test set
         df_train = df_normal.sample(frac=0.7)
@@ -34,27 +35,43 @@ class Dset_converter():
         df_an_train = df_anomalous.sample(frac=0.6)
         df_an_test = df_anomalous.loc[~df_anomalous.index.isin(df_an_train.index)]
         dfs = [df_train, df_test, df_an_train, df_an_test]
-        for df in dfs:
-            print(df.describe())
         df_train = df_train.append(df_an_train)
-        df_test = df_test.appden(df_an_test)
+        df_test = df_test.append(df_an_test)
 
         # Split each set into samples and targets
         datasets = {
             "train" : {
-                "samples": df_train.drop("Labels", axis=1),
-                "targets": df_train.Labels
+                "samples": df_train.drop("labels", axis=1),
+                "targets": df_train.labels
             },
             "test": {
-                "samples": df_test.drop("Labels", axis=1),
-                "targets": df_test.Labels
+                "samples": df_test.drop("labels", axis=1),
+                "targets": df_test.labels
             }
         }
         
         # Store the resulting four datasets
         for dset in self.output_names.keys():
-            for key in dset.keys():
+            print(dset)
+            for key in self.output_names[dset].keys():
                 location = self.output_names[dset][key]
                 data = datasets[dset][key]
-                print(location)
-                print(data.head())
+                path = os.path.join(self.RAW_PATH, location)
+                print(key)
+                if verbose:
+                    print(os.path.join(self.RAW_PATH, location))
+                    print(data.head())
+                    print("Length = ", len(data))
+                data.to_hdf(path, key='data')
+                print("Stored {}_{} to {}".format(dset, key, path))
+
+                # Calculate md5 of hdf files
+                md5 = hashlib.md5()
+                with open(path, "rb") as f:
+                    for chunk in iter(lambda: f.read(1024 * 1024), b""):
+                        md5.update(chunk)
+                print(md5.hexdigest())
+
+if __name__ == "__main__":
+    converter = Dset_converter()
+    converter.convert_dataset()
